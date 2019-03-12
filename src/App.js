71491c52c1078999a7397d11d4786997a9fd19b6
Jake <jacobartz1987@gmail.com>
2019-03-12 14:22:58 -0600

    Passing methods into stateless components

--- src/App.js
+++ src/App.js
@@ -4,39 +4,50 @@ import Person from './Person/Person';
 
 class App extends Component {
   state = {
-    persons : [
-      {name: 'Terri', age: 25},
-      {name: 'Berri', age: 43},
-      {name: 'Merri', age: 34},
+    persons: [
+      { name: 'Terri', age: 25 },
+      { name: 'Berri', age: 43 },
+      { name: 'Merri', age: 34 },
     ],
-    otherState : 'some value'
+    otherState: 'some value'
   }
 
 
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     console.log('was clicked')
-    this.setState({persons : [
-      {name: 'Lerri', age: 25},
-      {name: 'Kerri', age: 43},
-      {name: 'Derri', age: 34},
-    ]
-   });
+    this.setState({
+      persons: [
+        { name: newName, age: 25 },
+        { name: 'Kerri', age: 43 },
+        { name: 'Derri', age: 34 },
+      ]
+    });
   };
 
 
-render () {
-return (
-  <div className="App">
-    <h1>More longer, friend, see that sky?</h1>
-    <p>This state really hurts!</p>
-    <button onClick={this.switchNameHandler}>Switch Name</button>
-    <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-    <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >I love sailing!</Person>
-    <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-  </div>
-);
-}
+  render() {
+    return (
+      <div className="App">
+        <h1>More longer, friend, see that sky?</h1>
+        <p>This state really hurts!</p>
+        <button onClick={() => this.switchNameHandler("Jauqin")}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+          click={this.switchNameHandler.bind(this, 'Jenn')}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age} >I love sailing!
+    </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+      </div>
+    );
+  }
 
 }
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,8 +3,8 @@ import React from 'react';
 const person = (props) => {
     return (
         <div>
-            <p>I'm a person? I've been here for {props.age} years. My name... is ..{props.name}</p>
-            <p>{props.children}</p>
+            <p onClick={ props.click }> I'm a person? I've been here for { props.age } years. My name... is ..{props.name}</p>
+            <p>{ props.children }</p>
         </div>
     )
 }
